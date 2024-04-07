afc2863b7fbfab9f34e1586c56cc7f5c2053e10f
jong-un <36259838+zwong91@users.noreply.github.com>
2024-04-07 17:15:43 +0800

    Update cargo-unc version to 0.7.0

--- examples/src/gen/adder.rs
+++ examples/src/gen/adder.rs
@@ -1,6 +1,6 @@
 pub type Pair = Vec<i64>;
 pub struct AbiClient {
-    pub contract: unc_workspaces::Contract,
+    pub contract: near_workspaces::Contract,
 }
 impl AbiClient {
     pub async fn add(&self, a: Pair, b: Pair) -> anyhow::Result<Pair> {
